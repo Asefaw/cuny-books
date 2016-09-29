a63e07aa6af2c723ff4d4aee97694394513a8f93
Asefaw <asefaw.mekuria@mail.citytech.cuny.edu>
2016-09-29 17:18:36 +0000

    update

--- models/Book.js
+++ models/Book.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const db = require('../models/db'); // for db connection
 
-const Schema = mongoose.Schema();
+const Book = mongoose.Schema();
 
-const bookSchema = new Schema{
+const bookSchema = new Book({
 	title: {type: String, required: true, lowercase: true},
 	isbn: {type: String, required: true},
 	autor: String,
 	price: Number,
 	type: String
-};
+});
 
 module.exports = mongoose.model('book', bookSchema);
