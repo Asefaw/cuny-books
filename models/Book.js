185ee7ce8658acbc4c957e3c326e00b3315b2f27
Asefaw <asefaw.mekuria@mail.citytech.cuny.edu>
2016-10-07 17:54:11 +0000

    working on Book

--- models/Book.js
+++ models/Book.js
@@ -4,11 +4,27 @@ const db = require('../database/db'); // for db connection
 const Book = mongoose.Schema();
 
 const bookSchema = new Book({
+	owner:String,
 	title: {type: String, required: true, lowercase: true},
 	isbn: {type: String, required: true},
 	autor: String,
 	price: Number,
-	type: String
+	type: String,
+	quantity: String
 });
 
 module.exports = mongoose.model('book', bookSchema);
+
+module.exports.saveBook = function(owner,newBook,callback){
+	newBook.save(callback);
+}
+
+module.exports.findBooksByISBN = function(isbn, callback){
+	var query = {isbn: isbn};
+	Book.findOne(query,callback);
+}
+
+module.exports.findBooksByTitle = function(title, callback){
+	var query = {title: title};
+	Book.findOne(query,callback);
+}
