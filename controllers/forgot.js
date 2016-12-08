aa597a24493f04286910745ca61942de91c010b8
Seung A Jang <jseunga@hotmail.com>
2016-12-08 11:05:29 -0500

    Update

--- controllers/forgot.js
+++ controllers/forgot.js
@@ -74,11 +74,12 @@ router.post('/forgot', function(req, res, next) {
 
       var mailOptions = {
         to: user.email,
-        from: 'passwordreset@demo.com',
         subject: 'CUNYBooks Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
           'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'Your password reset link will be expired in 1 hour. If expires, please request a new link below.\n' +
+          'http://localhost:5000/forgot\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
 
